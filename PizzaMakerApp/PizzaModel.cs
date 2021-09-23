cd9399ddb250c906d2af39098fea8e764dbfd7e1
ambbrun <90715719+ambbrun@users.noreply.github.com>
2021-09-23 14:42:20 -0400

    Add files via upload

---
+++ PizzaMakerApp/PizzaModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaMakerApp
+{
+    public class PizzaModel
+    {
+        public List<string> Toppings { get; set; }
+    }
+}
